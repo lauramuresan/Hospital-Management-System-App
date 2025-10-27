1355015c60766c7ce9bd7d0e7027b148bff019da
lauramuresan <lauramuresan420@gmail.com>
2025-10-27 21:20:02 +0200

    initial commit

--- src/main/java/com/example/Hospital/Management/System/HospitalManagementSystemApplication.java
+++ src/main/java/com/example/Hospital/Management/System/HospitalManagementSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HospitalManagementSystemApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementSystemApplication.class, args);
 	}
